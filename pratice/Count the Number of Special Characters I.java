57e9e90539dd24bd601a146540fa25514700694e
PraveenPraveenVenkat <praveenvenkatachalam2001@gmail.com>
2026-05-27 22:52:08 +0530

    feat: improve DSA pattern recognition

---
+++ pratice/Count the Number of Special Characters I.java
@@ -0,0 +1,25 @@
+//! 3120. Count the Number of Special Characters I
+class Solution {
+    public int numberOfSpecialChars(String word) {
+        StringBuilder sb = new StringBuilder();
+        HashSet<Character> set = new HashSet();
+        char[] ch = word.toCharArray();
+
+        // for (int i = 0; i < ch.length(); i++) {// for(int i:word)
+        for (char c : ch) {
+            set.add(c);
+        }
+
+        int count = 0;
+        for (char c = 'a'; c <= 'z'; c++) {
+
+            if (set.contains(c) && set.contains(Character.toUpperCase(c))) {
+
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+}
