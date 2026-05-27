427f45c35064dac518f3defac7888d2bbfa99179
PraveenPraveenVenkat <praveenvenkatachalam2001@gmail.com>
2026-05-27 11:51:52 +0530

    feat(dsa): complete binary search and assignment problems

--- DSA/Binary Search/Find First and Last Position of Element in Sorted Array.java
+++ DSA/Binary Search/Find First and Last Position of Element in Sorted Array.java
@@ -1,1 +1,23 @@
-//! Leetcode 34
+//! 34 leetcode 34. Find First and Last Position of Element in Sorted Array 
+class Solution {
+    public int[] searchRange(int[] nums, int target) {
+    
+    int start=binarySearch(nums,target);
+    if(start==nums.length||nums[start]!= target) return new int[] {-1,-1};
+    return new int []{start,binarySearch(nums,target+1)-1};
+    }
+
+    private static int binarySearch(int nums[], int target){
+           int left = 0, right = nums.length;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+
+        }
+        return left;
+    }
+}
--- DSA/Binary Search/Sqrt(x).java
+++ DSA/Binary Search/Sqrt(x).java
@@ -19,3 +19,18 @@ class Solution {
          return right;
     }
 }
+Brut Force Solution 
+class Solution {
+    public int mySqrt(int x) {
+    
+      if(x<2){
+        return x;
+      }
+      int i=2;
+      while(i*i <=x){
+        i+=1;
+      }
+return i-1;
+    
+    }
+}
