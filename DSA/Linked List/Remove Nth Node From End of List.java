1cd07330f432c0235e0c6fb90108737789b23b43
PraveenPraveenVenkat <praveenvenkatachalam2001@gmail.com>
2026-06-23 21:03:21 +0530

    feat(dsa): Linked List Completed 4 Problem in VDSA

---
+++ DSA/Linked List/Remove Nth Node From End of List.java
@@ -0,0 +1,40 @@
+//! 19. Remove Nth Node From End of List
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummy= new ListNode(-1,head);
+        // dummy=head;
+        ListNode slow=dummy,fast=dummy;
+        for(int i=0;i<=n;i++)  fast=fast.next;
+        while(fast!=null){
+         fast=fast.next;
+         slow=slow.next;
+        }
+        slow.next=slow.next.next;
+        return dummy.next;
+    }
+}
+or 
+//?
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+
+        ListNode dummy = new ListNode(-1);
+        dummy.next = head;
+
+        ListNode slow = dummy;
+        ListNode fast = dummy;
+
+        for (int i = 0; i <= n; i++) {
+            fast = fast.next;
+        }
+
+        while (fast != null) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+
+        slow.next = slow.next.next;
+
+        return dummy.next;
+    }
+}
