2fe33167a928486169c53fc30109ecc08fe7b2b6
PraveenPraveenVenkat <praveenvenkatachalam2001@gmail.com>
2026-06-26 20:54:47 +0530

    feat(dsa): Partition List  in VDSA

---
+++ DSA/Linked List/Partition List.java
@@ -0,0 +1,34 @@
+//! 86. Partition List
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode partition(ListNode head, int x) {
+        ListNode lesser = new ListNode(0);
+        ListNode greater = new ListNode(0);
+
+        ListNode lHead = lesser, gHead = greater;
+
+        while (head != null ) {
+            if (head.val < x) {
+                lHead.next=head;
+                lHead=lHead.next;
+            }else{
+                gHead.next=head;
+                gHead=gHead.next;
+            }
+            head=head.next;
+       
+            gHead.next=null;
+            lHead.next=greater.next;
+         }
+        return lesser.next;
+    }
+}
