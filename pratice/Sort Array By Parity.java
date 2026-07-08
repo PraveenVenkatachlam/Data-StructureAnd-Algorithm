3d70415de59bf14074ebd722b7ee539d170c8ee5
PraveenPraveenVenkat <praveenvenkatachalam2001@gmail.com>
2026-07-08 21:39:14 +0530

    feat:Sort Array By Parity

---
+++ pratice/Sort Array By Parity.java
@@ -0,0 +1,18 @@
+//! 905. Sort Array By Parity
+class Solution {
+    public int[] sortArrayByParity(int[] nums) {
+
+        int left = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] % 2 == 0) {
+                int temp = nums[left];
+                nums[left] = nums[i];
+                nums[i] = temp;
+                left++;
+            }
+        }
+
+        return nums;
+    }
+}
