9bb5cfbb454421641e4907fba004ee8f2eff66e5
PraveenPraveenVenkat <praveenvenkatachalam2001@gmail.com>
2026-07-09 22:17:07 +0530

    feat:Remove Duplication From Sorted

---
+++ pratice/Remove Duplicates from Sorted List II.java
@@ -0,0 +1,43 @@
+
+ class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null || head.next == null) return head;
+
+        ListNode dummy = new ListNode(-1); // Dummy node to handle head removals
+        dummy.next = head;
+        ListNode prev = dummy;
+        ListNode cur = head;
+
+        while (cur != null && cur.next != null) {
+            if (cur.val == cur.next.val) {
+                // Skip all nodes with the same value
+                while (cur.next != null && cur.val == cur.next.val) {
+                    cur = cur.next;
+                }
+                prev.next = cur.next; // Remove duplicates
+            } else {
+                prev = prev.next; // Move to next distinct node
+            }
+            cur = cur.next;
+        }
+
+        return dummy.next;
+    }
+}
+// class Solution {
+//     public ListNode deleteDuplicates(ListNode head) {
+
+//        List<Integer> res=new LinkedList<>();
+//     //    while()
+//        for(int i=0;i<head.length;i++){
+//           if(head.val()==head.next.val()){
+//             head.next=head.next.next;
+//             res.add();
+//           }
+//        } 
+//      return res;
+//     }
+// }
+
+
+
