c88d5994e144a53d027ac87710644712d6964183
PraveenPraveenVenkat <praveenvenkatachalam2001@gmail.com>
2026-07-13 20:16:27 +0530

    feat:Stack Completed in Vdsa

---
+++ DSA/Stack/Basic Calculator.java
@@ -0,0 +1,32 @@
+//! 224 Basic Calculator
+Amazon,Microsoft, Google,etc
+class Solution {
+    public int calculate(String s) {
+        Stack<Integer> stack = new Stack<>();
+        int num = 0, result = 0, sign = 1;
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (Character.isDigit(ch)) {
+                num = num *10 +(ch - '0');
+            } else if (ch == '+' || ch == '-') {
+                result += sign * num;
+                num = 0;
+                sign = (ch == '+') ? 1 : -1;
+            } else if (ch == '(') {
+                stack.push(result);
+                stack.push(sign);
+               result = 0;
+                sign = 1;
+            } else if (ch == ')') {
+                result += sign * num;
+                num = 0;
+                result *= stack.pop();
+                result += stack.pop();
+               
+            }
+
+        }
+        result += sign * num;
+        return result;
+    }
+}
--- DSA/Stack/Largest Rectangle in Histogram.java
+++ DSA/Stack/Largest Rectangle in Histogram.java
@@ -1,3 +1,47 @@
-
+//! 84. Largest Rectangle in Histogram
 // Asked in Apple, meta etc
 
+
+class Solution {
+    public int largestRectangleArea(int[] heights) {
+        int n = heights.length;
+        int[] pse = getpse(heights, n);
+        int[] nse = getnse(heights, n);
+        // nse(height);
+        int maxheight = 0;
+        for (int i = 0; i < n; i++) {
+            int width = nse[i] - pse[i] - 1;
+            int area = heights[i] * width;
+            maxheight = Math.max(maxheight, area);
+        }
+        return maxheight;
+
+    }
+
+    // int[] pse= getpse(heigth,n);
+    private int[] getpse(int[] heights, int n) {
+        Stack<Integer> stack = new Stack<>();
+        int[] pse = new int[n];
+        for (int i = 0; i < n; i++) {
+            while (!stack.isEmpty() && heights[stack.peek()] >= heights[i])
+                stack.pop();
+            pse[i] = stack.isEmpty() ? -1 : stack.peek();
+            stack.push(i);
+
+        }
+        return pse;
+    }
+
+    private int[] getnse(int[] heights, int n) {
+        Stack<Integer> stack = new Stack<>();
+        int[] gse = new int[n];
+        for (int i = n - 1; i >= 0; i--) {
+            while (!stack.isEmpty() && heights[stack.peek()] >= heights[i])
+                stack.pop();
+            gse[i] = stack.isEmpty() ? n : stack.peek();
+
+            stack.push(i);
+        }
+        return gse;
+    }
+}
