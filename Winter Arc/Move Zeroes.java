00b07516299c913040060bc1735811ac0a1cf778
PraveenPraveenVenkat <praveenvenkatachalam2001@gmail.com>
2026-09-05 22:44:03 +0530

    feat:Day 3

---
+++ Winter Arc/Move Zeroes.java
@@ -0,0 +1,18 @@
+//! 283. Move Zeroes
+//TC:O(N)
+//SC:O(1)
+class Solution {
+    public void moveZeroes(int[] nums) {
+         int left=0;
+      
+           for(int right=0;right<nums.length;right++){
+            if(nums[right]!=0){
+                int temp=nums[right];
+                nums[right]=nums[left];
+                nums[left]=temp;
+                left++;
+            
+           }
+        }
+    }
+}
